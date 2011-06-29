d3cc365903b30ea2480bf89c488d76014937a401
Zhong <zhong@swimconnection.com>
2011-06-30 00:12:25 +0800

    all the others files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstProject::Application.config.secret_token = '3d814cbb2785627aeb10371ca96e2450ab2dd095e03290c5fef8301fc2feb0841635d7fae4776cb695822d953be30c994d6abf01740c7573e500bcf54ff591ca'
